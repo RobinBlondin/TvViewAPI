9fdba742fc6ccf7a42be73f59159499d795cdf5f
Robin Blondin <robin.blondin@protonmail.com>
2025-02-01 23:38:28 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/tvviewapi/TvViewApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tvviewapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TvViewApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TvViewApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/tvviewapi/TvViewApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tvviewapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TvViewApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
