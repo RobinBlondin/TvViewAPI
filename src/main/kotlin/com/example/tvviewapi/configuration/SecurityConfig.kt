2fb913a31c176e302e9de9b4b6fa54e064805f2f
Robin Blondin <robin.blondin@protonmail.com>
2025-02-02 00:30:45 +0100

    Set up basic security config with oauth2

---
+++ src/main/kotlin/com/example/tvviewapi/configuration/SecurityConfig.kt
@@ -0,0 +1,22 @@
+package com.example.tvviewapi.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
+        http
+            .authorizeHttpRequests { auth ->
+                auth.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .oauth2Login {
+
+            }
+            .build()
+}
