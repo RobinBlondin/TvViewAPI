f8ff565680b32fb4846836beede4289b8e722c51
Robin Blondin <robin.blondin@protonmail.com>
2025-02-02 15:05:32 +0100

    Added field to entity

--- src/main/kotlin/com/example/tvviewapi/entity/User.kt
+++ src/main/kotlin/com/example/tvviewapi/entity/User.kt
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails
 @Entity
 class User (
     private val email: String = "",
+    private val displayName: String = "",
     private var enable: Boolean = true
 ): BaseEntity(), UserDetails {
     override fun getUsername(): String = email
