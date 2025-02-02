23cabd56e209f3efbbe2f94bbc00e74ba0a6bdd3
Robin Blondin <robin.blondin@protonmail.com>
2025-02-02 14:48:47 +0100

    Added swagger and created user entity

---
+++ src/main/kotlin/com/example/tvviewapi/entity/User.kt
@@ -0,0 +1,26 @@
+package com.example.tvviewapi.entity
+
+import jakarta.persistence.*
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+@Entity
+class User (
+    private val email: String = "",
+    private var enable: Boolean = true
+): BaseEntity(), UserDetails {
+    override fun getUsername(): String = email
+
+    override fun getPassword(): String? = null
+    @Transient
+    override fun getAuthorities(): MutableList<GrantedAuthority> = mutableListOf(SimpleGrantedAuthority("ROLE_USER"))
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = enable
+}
