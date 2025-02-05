2519d6b0baf482103ae4425a3c9a735df994de2d
Robin Blondin <robin.blondin@protonmail.com>
2025-02-05 08:46:06 +0100

    Removed annotation

--- src/main/kotlin/com/example/tvviewapi/controller/UserController.kt
+++ src/main/kotlin/com/example/tvviewapi/controller/UserController.kt
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = ["*"])
 class UserController(val userService: UserService) {
 
     @PostMapping("/create")
