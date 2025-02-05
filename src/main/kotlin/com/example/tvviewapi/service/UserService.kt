54b3de54a0d74bd484245ff1f0897f5aa848afcb
Robin Blondin <robin.blondin@protonmail.com>
2025-02-05 08:45:31 +0100

    MAde service method for creating users

--- src/main/kotlin/com/example/tvviewapi/service/UserService.kt
+++ src/main/kotlin/com/example/tvviewapi/service/UserService.kt
@@ -4,24 +4,33 @@ import com.example.tvviewapi.dto.UserDto
 import com.example.tvviewapi.entity.User
 import com.example.tvviewapi.mapper.UserMapper
 import com.example.tvviewapi.repository.UserRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
+import java.util.*
 import kotlin.jvm.optionals.getOrNull
 
 @Service
 class UserService(val userRepository: UserRepository, val userMapper: UserMapper) {
     fun findAll(): List<UserDto> = userRepository.findAll().map { userMapper.toDto(it) }
 
-    fun findUserByEmail(email: String): UserDto? {
+    fun findUserByEmail(email: String): Optional<UserDto> {
         val user = userRepository.getUserByEmail(email)
 
         if (user.isPresent) {
-            return userMapper.toDto(user.get())
+            return Optional.of(userMapper.toDto(user.get()))
         }
-        return null
+        return Optional.empty()
     }
 
-    fun create(userDto: UserDto): UserDto {
+    fun create(userDto: UserDto): Optional<UserDto> {
+        if(findUserByEmail(userDto.email).isPresent) {
+            return Optional.empty()
+        }
+
         val user = userMapper.toEntity(userDto)
-        return userMapper.toDto(userRepository.save(user))
+        val saved = userRepository.save(user)
+
+        return Optional.of(userMapper.toDto(saved))
     }
+
 }
