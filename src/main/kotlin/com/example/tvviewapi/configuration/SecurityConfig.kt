ee46a925322299f4fa8778f2e9102536344390b8
Robin Blondin <robin.blondin@protonmail.com>
2025-02-08 01:31:16 +0100

    added oauth2 login and resource server

--- src/main/kotlin/com/example/tvviewapi/configuration/SecurityConfig.kt
+++ src/main/kotlin/com/example/tvviewapi/configuration/SecurityConfig.kt
@@ -1,31 +1,66 @@
 package com.example.tvviewapi.configuration
 
+import com.example.tvviewapi.service.UserService
+import lombok.RequiredArgsConstructor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val userService: UserService
+) {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
         http
-            .csrf { it.disable() }
+            .csrf { csrf -> csrf.disable() }
             .authorizeHttpRequests { auth ->
-                auth.requestMatchers(
-                    "/swagger-ui.html",
-                    "/swagger-ui/**",
-                    "/v3/api-docs/**",
-                    "/api/**"
-                ).permitAll()
+                auth
+                    .requestMatchers(
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/api/users/test"
+                    ).permitAll()
                     .anyRequest().authenticated()
             }
-            .oauth2Login {
-                it.defaultSuccessUrl("/")
-                it.failureUrl("/login-error")
+            .oauth2Login{
+                login -> login
+                        .defaultSuccessUrl("/api/users/test", true)
+                        .failureUrl("/swagger-ui.html")
+            }
+            .oauth2ResourceServer { oauth2 ->
+                oauth2.jwt { jwt ->
+                    jwt.jwtAuthenticationConverter { token ->
+                        val claims = token.claims
+                        val email = claims["email"] as? String
+
+                        println("Email: $email")
+
+                        if (email != null && userService.isRegisteredUser(email)) {
+                            UsernamePasswordAuthenticationToken(
+                                email,
+                                token,
+                                listOf(SimpleGrantedAuthority("USER"))
+                            )
+                        } else {
+                            throw UsernameNotFoundException("Unauthorized user")
+                        }
+                    }
+                }
             }
             .build()
+
+    @Bean
+    fun jwtDecoder(): JwtDecoder {
+        return NimbusJwtDecoder.withJwkSetUri("https://www.googleapis.com/oauth2/v3/certs").build()
+    }
 }
