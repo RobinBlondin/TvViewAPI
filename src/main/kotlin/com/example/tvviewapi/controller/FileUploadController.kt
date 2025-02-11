af2a735280a20292ac5bff008e4445368da6b63c
Robin Blondin <robin.blondin@protonmail.com>
2025-02-11 22:17:01 +0100

    Added controller for uploading files

---
+++ src/main/kotlin/com/example/tvviewapi/controller/FileUploadController.kt
@@ -0,0 +1,37 @@
+package com.example.tvviewapi.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.time.LocalDateTime
+import kotlin.io.path.exists
+
+@RestController
+@RequestMapping("/api/files")
+class FileUploadController {
+
+      private val uploadDir = Paths.get("uploads/slides")
+
+      init {
+            if(!uploadDir.exists()) {
+                  Files.createDirectory(uploadDir)
+            }
+      }
+
+      @PostMapping("upload")
+      fun uploadFile(@RequestParam("file") file: MultipartFile): ResponseEntity<String> {
+            val timestamp = LocalDateTime.now().toString().replace(":", "-")
+            val fileName = "$timestamp-${file.originalFilename}"
+            val filePath = uploadDir.resolve(fileName)
+
+            Files.copy(file.inputStream, filePath)
+
+            val storedUrl = "/$filePath/$fileName"
+            return ResponseEntity.ok().body(storedUrl)
+      }
+}
