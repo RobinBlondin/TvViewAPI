ba1de5c8f27be5433da8317ca0690f72fed12b8a
Robin Blondin <robin.blondin@protonmail.com>
2025-04-12 17:12:14 +0200

    Added service for fetching highway bridge openings

---
+++ src/main/kotlin/com/example/tvviewapi/service/HighwayBridgeOpeningService.kt
@@ -0,0 +1,18 @@
+package com.example.tvviewapi.service
+
+import com.example.tvviewapi.dto.HighwayBridgeOpenings
+import com.example.tvviewapi.dto.PlannedOpeningDto
+import org.springframework.stereotype.Service
+
+@Service
+class HighwayBridgeOpeningService(
+      val jsonService: JsonService
+) {
+      val url = "https://api.sodertalje.se/getBridgeplanned"
+
+      fun getBridgeOpenings(): List<PlannedOpeningDto> {
+            val json = jsonService.fetch(url)
+            return jsonService.parse<List<PlannedOpeningDto>>(json)
+      }
+
+}
