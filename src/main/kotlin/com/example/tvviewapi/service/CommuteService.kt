e82cdcc26b44785967c53986d33bc72c85bc6982
Robin Blondin <robin.blondin@protonmail.com>
2025-04-12 17:13:20 +0200

    Updated commuteservice to fit the new jsonService

--- src/main/kotlin/com/example/tvviewapi/service/CommuteService.kt
+++ src/main/kotlin/com/example/tvviewapi/service/CommuteService.kt
@@ -1,31 +1,24 @@
 package com.example.tvviewapi.service
 
-import com.example.tvviewapi.dto.DepartureDto
 import com.example.tvviewapi.dto.DepartureBoard
-import io.github.cdimascio.dotenv.Dotenv
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import com.example.tvviewapi.dto.DepartureDto
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import kotlinx.serialization.json.*
 
 @Service
 class CommuteService(
-      val jsonParser: Json = Json { ignoreUnknownKeys = true },
-      val commuteStopId: String? = System.getenv("COMMUTE_STOP_ID"),
-      val commuteApiKey: String? = System.getenv("COMMUTE_API_KEY")
+      val jsonService: JsonService,
+      @Value("\${COMMUTE_STOP_ID}") val commuteStopId: String?,
+      @Value("\${COMMUTE_API_KEY}") val commuteApiKey: String?
 ) {
 
-      private fun fetchJson(url: String): String {
-            val client = OkHttpClient()
-            val request = Request.Builder().url(url).build()
-            client.newCall(request).execute().use { response ->
-                  return response.body?.string() ?: ""
-            }
-      }
 
       fun getDepartures(): List<DepartureDto> {
-            val json = fetchJson("https://api.resrobot.se/v2.1/departureBoard?id=${commuteStopId}&format=json&accessId=${commuteApiKey}")
-            return  jsonParser.decodeFromString<DepartureBoard>(json).departures
+            val json = jsonService.fetch("https://api.resrobot.se/v2.1/departureBoard?id=${commuteStopId}&format=json&accessId=${commuteApiKey}")
+            println(json)
+            println(commuteStopId)
+            println(commuteApiKey)
+            return  jsonService.parse<DepartureBoard>(json).departures
       }
 
 }
